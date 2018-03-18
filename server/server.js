73b341459850958cb91faf6b83bfde234d54d3a6
User 1 <user1@localhost>
2018-03-18 17:22:07 +0000

    Added createMessage and newMessage

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
@@ -12,9 +12,19 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.on('createMessage', (message) => {
+    console.log(message);
+
+    socket.emit('newMessage', {
+      name: message.name,
+      text: message.text,
+      createdAt: new Date().toString(),
+    });
+  });
+
   socket.on('disconnect', () => {
     console.log('User disconneted');
-  })
+  });
 });
 
 app.use(express.static(publicPath));
