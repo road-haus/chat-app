abd95597aa4af1637aba7d89c7c4da285bcc7f38
User 1 <user1@localhost>
2018-03-20 19:14:32 +0000

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -12,6 +12,20 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  // socket.emit from Admin text Welcome to the chat app
+  // socket.broadcast.emit from Admin text New user joined
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime(),
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined!',
+    createdAt: new Date().getTime(),
+  });
+
   socket.on('createMessage', (message) => {
     console.log(message);
     io.emit('newMessage', {
@@ -19,6 +33,11 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime(),
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime(),
+    // });
   });
 
   socket.on('disconnect', () => {
